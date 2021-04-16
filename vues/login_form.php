e8e7c194cf198bb3251c68eca8ab24806b9a6d13
ernesto57 <ernesto57370@gmail.com>
2021-04-16 12:29:03 +0200

    css modif

--- vues/login_form.php
+++ vues/login_form.php
@@ -13,6 +13,7 @@
 <html>
     <head>
         <title>Connexion-Inscription</title>
+        <link rel="icon" type="image/png" href="./../images/favicon.png" />
         <link rel="stylesheet" type="text/css" href="../styles/login_form.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
