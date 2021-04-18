42979e74ca8dce21def135f66a673cd4e1ca8bc3
Neikyru <jaber.neiky@gmail.com>
2021-04-18 20:37:27 +0200

    push

--- vues/lot_dynamique_client.php
+++ vues/lot_dynamique_client.php
@@ -25,13 +25,11 @@ else if(isset($_GET['lot_sold'])){
         <?php 
             $sql ="SELECT * FROM v_affichage_client WHERE lot_id= ".$lot;
            // $sql ="SELECT afficher_lot_client(". $lot . ")";
-            echo $sql;
+           // echo $sql;
             $req=$bdd->prepare($sql);
             $req->execute();
             while ($row = $req->fetch(PDO::FETCH_ASSOC)) {
-                echo "<option value='" . $row['lot_id'] . "'>";
                 echo $row['lot_id']." disponible jusque : " .$row['lot_date_fin_vente'];
-                echo "</option>";   
                     }
         ?>
                 
@@ -41,8 +39,31 @@ else if(isset($_GET['lot_sold'])){
         <?php 
         $bdd = Bdd::getBdd();
         $sql = "SELECT lot_id FROM t_lot_lot WHERE ";
-        echo $sql;
-        ?>
+        echo $sql; 
+        $req=$bdd->prepare($sql);
+        $req->execute();
+        while ($row = $req->fetch(PDO::FETCH_ASSOC)) 
+        {
+
+            //Confirmer son achat(a)(page de lot dynamique)
+            if( $row['gagnant'] = $row['pseudo'])
+            echo "Vous êtes éligible à l'achat du lot" .$row['lot_id']."Vous devez confirmer son achat";
+            //button pour changer la confirmation de faux à vrai
+            echo "<button onclick=" pro_confirmation FROM t_proposition_achat_pro WHERE pro.lot_id=lot_id into">Click Here</button> "
+
+
+              
+        
+         //Proposer un prix(page de lot dynamique)
+         
+
+         //Modifier proposition (à la hausse)(page de lot dynamique)
+
+        
+
+         //Voir le résultat de la vente(php)(page de lot dynamique)
+        }
+        ?> 
         </div>
     </body>
 </html>
