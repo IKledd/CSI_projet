47adf4f90dd0176d9149fedcb0bc8b762336454a
ernesto57 <ernesto57370@gmail.com>
2021-04-19 06:27:20 +0200

    modif dynamique cloent

--- vues/lot_dynamique_client.php
+++ vues/lot_dynamique_client.php
@@ -3,37 +3,62 @@ require "../fonctions/bdd.php";
 require "../fonctions/statusClient.php";
 $bdd = Bdd::getBdd();
 $lot = 1; 
-/*if(isset($_GET['lot_on_sale'])){
+if(isset($_GET['lot_on_sale'])){
    $lot = $_GET['lot_on_sale'];
+   $etat='en vente';
 }
 else if(isset($_GET['lot_sold'])){
     $lot = $_GET['lot_sold'];
-} */
+    $etat='vendu';
+} 
+else{
+    //header('Location: ./general_client.php');
+}
 ?>
 
 
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Informations de votre lot</title>
+        <title>Lot numéro <?php echo $lot; ?></title>
        <!-- <link rel="stylesheet" type="text/css" href=""> -->
         <meta charset="utf-8"/>
     </head>
     <body>
     
-        <div id="showlot"> Lot numero 
+        <p id="titlz">Lot numéro <?php echo $lot; ?></p>
         <?php 
             $sql ="SELECT * FROM v_affichage_client WHERE lot_id= ".$lot;
            // $sql ="SELECT afficher_lot_client(". $lot . ")";
            // echo $sql;
             $req=$bdd->prepare($sql);
             $req->execute();
-            while ($row = $req->fetch(PDO::FETCH_ASSOC)) {
-                echo $row['lot_id']." disponible jusque : " .$row['lot_date_fin_vente'];
-                    }
+            $result = $req->fetch(PDO::FETCH_ASSOC);
+           echo($result['lot_date_debut_vente']);
+
         ?>
-                
-            </div>
+        <table>
+            <thead>
+                <tr>
+                <th colspan="2">Informations sur le lot numéro <?php echo $lot . " (" . $etat . ")"; ?></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Date de début de vente</td>
+                    <td><?php echo $result['lot_date_debut_vente'];?></td>
+                </tr>
+                <tr>
+                    <td>Date de fin de vente</td>
+                    <td><?php echo $result['lot_date_fin_vente'];?></td>
+                </tr>
+                <tr>
+                    <td>Prix estimé</td>
+                    <td><?php echo $result['lot_prix_estime'];?></td>
+                </tr>
+            </tbody>
+        </table>
+           <!--  </div>
         
         <div id="showinfo">
         <?php 
@@ -63,7 +88,7 @@ else if(isset($_GET['lot_sold'])){
 
          //Voir le résultat de la vente(php)(page de lot dynamique)
         }
-        ?> 
+        ?>  -->
         </div>
     </body>
 </html>
